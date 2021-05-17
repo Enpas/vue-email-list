4ac96012971fabe2448e90aae1bffba79d3bc06c
Enpas <enniopas2002@libero.it>
2021-05-17 17:05:02 +0200

    commit (non stampa a schermo la lista)

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const app = new Vue({
+
+  el: '#app',
+  data: {
+    arrEmail: [],
+  },
+
+  mounted() {
+    
+    // invoco la funzione emailRandom
+    this.emailRandom('https://flynn.boolean.careers/exercises/api/random/mail');
+  },
+
+  methods: {
+
+    emailRandom(url) {
+      axios.get(url)
+        .then(resp => {
+          this.arrEmail.push(resp.data.response);
+          if(this.arrEmail.length < 10) this.emailRandom(url); 
+        })
+
+        .catch(err => {
+          console.log(err + ' Page not found');
+        });
+     } 
+  } 
+
+}); 
